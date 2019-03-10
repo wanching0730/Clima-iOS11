967b30717c07f3c6b655a07e2bc32dd5d545987e
wanching_97 <wanching_97@hotmail.com>
2019-03-10 10:33:49 +0800

    last

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -16,7 +16,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate, Change
     
     //Constants
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather"
-    let APP_ID = "e72ca729af228beabd5d20e3b7749713"
+    let APP_ID = "be367036e2a978a09dae57b03ec2aae4"
     
 
     //TODO: Declare instance variables here
